60423a7d1b8e5d743c8e44957b78d23191f69ba1
Matias001 <167754790+MateuszPazio@users.noreply.github.com>
2025-12-05 11:16:08 +0100

    Ujednolicenie wersji wtyczki #7
    Add plugin name and version extraction from metadata.txt

--- __init__.py
+++ __init__.py
@@ -1,3 +1,15 @@
+import os
+
+"""Wersja wtyczki"""
+PLUGIN_NAME = ''
+PLUGIN_VERSION = ''
+with open(os.path.join(os.path.dirname(__file__), 'metadata.txt'), 'r') as pluginMetadataFile:
+    for line in pluginMetadataFile:
+        if line.startswith('name='):
+            PLUGIN_NAME = line.split('=')[1].strip()
+        elif line.startswith('version='):
+            PLUGIN_VERSION = line.split('=')[1].strip()
+
 def classFactory(iface):
     # Load the RevealAddressPlugin class from the RevealAddressPlugin.py file
     from .RevealAddressPlugin import RevealAddressPlugin
