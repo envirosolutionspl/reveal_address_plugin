7732629a9d613bf84c3dd297fbf128935f91932b
Matias001 <167754790+MateuszPazio@users.noreply.github.com>
2025-12-05 11:16:48 +0100

    Ujednolicenie wersji, pozbycie się niepotrzbnej warstwy i nazyw #7
    Removed plugin metadata loading from file.

--- RevealAddressPlugin.py
+++ RevealAddressPlugin.py
@@ -8,16 +8,6 @@ from qgis.PyQt.QtGui import QIcon
 import json
 import os
 
-"""Wersja wtyczki"""
-PLUGIN_NAME = ''
-PLUGIN_VERSION = ''
-with open(os.path.join(os.path.dirname(__file__), 'metadata.txt'), 'r') as pluginMetadataFile:
-    for line in pluginMetadataFile:
-        if line.startswith('name='):
-            PLUGIN_NAME = line.split('=')[1].strip()
-        elif line.startswith('version='):
-            PLUGIN_VERSION = line.split('=')[1].strip()
-
 class RevealAddressMapTool(QgsMapToolEmitPoint):
     def __init__(self, canvas):
         self.canvas = canvas
