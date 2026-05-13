bf1eeb6271b5187ff242cea1bf9767cb39bf786c
wojtek19022 <113895660+wojtek19022@users.noreply.github.com>
2026-05-13 17:03:21 +0200

    Naprawa działania QGIS Feed przy starcie wtyczki

--- constants.py
+++ constants.py
@@ -5,3 +5,21 @@ MIN_LON, MAX_LON = 14.07, 24.03
 PLUGIN_ICON = './icons/plugin_icon.png'
 
 EPSG = 4326
+
+FEED_URL = 'https://qgisfeed.envirosolutions.pl/'
+
+INDUSTRIES = {
+    "999": "Nie wybrano",
+    "e": "Energetyka/OZE",
+    "u": "Urząd",
+    "td": "Transport/Drogi",
+    "pg": "Planowanie/Geodezja",
+    "wk": "WodKan",
+    "s": "Środowisko",
+    "rl": "Rolnictwo/Leśnictwo",
+    "tk": "Telkom",
+    "edu": "Edukacja",
+    "i": "Inne",
+    "it": "IT",
+    "n": "Nieruchomości"
+}
